f07fc2e517c23f6d696e188380930ad9ce98589f
Fernando Stahelin <fernandostahelin@gmail.com>
2024-11-13 20:47:17 -0300

    added DDL to long process data table

--- data_dump/create_table_long_process_data.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS long_process_data;
-
-CREATE TABLE long_process_data(
-    seq_num SERIAL PRIMARY KEY,
-    execution_num INT NOT NULL,
-    sub_process_desc VARCHAR(255) NOT NULL,
-    record_create_dtm TIMESTAMP NOT NULL,
-    record_create_username VARCHAR(255) NOT NULL
-);
--- data_dump/northwind_source.sql
+++ data_dump/northwind_source.sql
@@ -36,6 +36,16 @@ DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS region;
 DROP TABLE IF EXISTS employees;
 
+DROP TABLE IF EXISTS long_process_data;
+
+CREATE TABLE long_process_data(
+    seq_num SERIAL PRIMARY KEY,
+    execution_num INT NOT NULL,
+    sub_process_desc VARCHAR(255) NOT NULL,
+    record_create_dtm TIMESTAMP NOT NULL,
+    record_create_username VARCHAR(255) NOT NULL
+);
+
 --
 -- Name: categories; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
